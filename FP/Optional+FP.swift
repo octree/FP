ac381f7add9c1078e8e17d7c74f8231b5556670b
Octree <fouljz@gmail.com>
2018-09-01 16:06:01 +0800

    ✨ Optional +  Monad

--- FP/Optional+FP.swift
+++ FP/Optional+FP.swift
@@ -11,3 +11,20 @@ import Foundation
 public func <^> <T, U>(f: (T) -> U, a: T?) -> U? {
     return a.map(f)
 }
+
+
+public func >>- <T, U>(a: T?, f: (T) -> U?) -> U? {
+    return a.flatMap(f)
+}
+
+public func -<< <T, U>(f: (T) -> U?, a: T?) -> U? {
+    return a.flatMap(f)
+}
+
+public func >-> <T, U, V>(f: @escaping (T) -> U?, g: @escaping (U) -> V?) -> (T) -> V? {
+    return { x in f(x) >>- g }
+}
+
+public func <-< <T, U, V>(f: @escaping (U) -> V?, g: @escaping (T) -> U?) -> (T) -> V? {
+    return { x in g(x) >>- f }
+}
